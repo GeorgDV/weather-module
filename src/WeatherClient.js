48971bf879728cc461e20346976182b50b84f231
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-05-20 14:46:02 +0300

    Add caching

--- src/WeatherClient.js
+++ src/WeatherClient.js
@@ -1,6 +1,14 @@
 const https = require('https');
+const fs = require('fs');
+const util = require('util');
 const WeatherRequests = require('./WeatherRequests');
 
+//TIME SET TO UPDATE CACHE IN EVERY 15 MINUTES
+const time = 900000;
+
+let weatherInfoCacheTimer = 0;
+let tempCacheTimer = 0;
+getCacheTimers();
 
 //city - city name as full or partial string
 const getWoeID = async (city) => {
@@ -18,13 +26,60 @@ const getWoeID = async (city) => {
     return woeid;
 }
 
-//woeID - Where On Earth ID - get via getWoeID
+//city - city name as full or partial string
 //day - array id, days start from current day (0), range 0-5
-const getWeatherInfo = async (city, day) => {
+//woeID - Where On Earth ID - get via getWoeID
+const getWeatherWithCache = async (city, day) => {
+    const now = new Date().getTime();
+    let weatherInfo = {};
+    if (await fs.existsSync(__dirname + '/cache/weatherInfoCache.txt')) {
+        weatherInfo = await fs.readFileSync( __dirname + '/cache/weatherInfoCache.txt');
+        weatherInfo = weatherInfo.toString('utf-8');
+    }
+    
+    if (weatherInfoCacheTimer < now) {
+        weatherInfo = await getWeather(city, day);
+        await fs.writeFileSync(__dirname + '/cache/weatherInfoCache.txt', util.inspect(weatherInfo), 'utf-8');
+        console.log('Updated Weather Info Cache!');
+
+        weatherInfoCacheTimer = getNewCacheTimer(time, weatherInfoCacheTimer);
+        await fs.writeFileSync(__dirname + '/cache/cacheTimers/weatherInfoCacheTimer.txt', weatherInfoCacheTimer, 'utf-8');
+    }
+
+    return weatherInfo;
+}
+
+//city - city name as full or partial string
+//day - array id, days start from current day (0), range 0-5
+//woeID - Where On Earth ID - get via getWoeID
+const getTempWithCache = async (city, day) => {
+    const now = new Date().getTime();
+    let tempInfo = {};
+    if (await fs.existsSync(__dirname + '/cache/tempCache.txt')) {
+        tempInfo = await fs.readFileSync( __dirname + '/cache/tempCache.txt');
+        tempInfo = tempInfo.toString('utf-8');
+    }
+    
+    if (tempCacheTimer < now) {
+        tempInfo = await getTemp(city, day);
+        await fs.writeFileSync(__dirname + '/cache/tempCache.txt', util.inspect(tempInfo), 'utf-8');
+        console.log('Updated Temperature Info Cache!');
+
+        tempCacheTimer = getNewCacheTimer(time, tempCacheTimer);
+        await fs.writeFileSync(__dirname + '/cache/cacheTimers/tempCacheTimer.txt', tempCacheTimer, 'utf-8');
+    }
+
+    return tempInfo;
+}
+
+//city - city name as full or partial string
+//day - array id, days start from current day (0), range 0-5
+//woeID - Where On Earth ID - get via getWoeID
+const getWeather = async (city, day) => {
     let weatherInfo = {};
 
     if (day > 5 || day < 0) {
-        console.error("DayID must range between 0-5.");
+        console.error("Given DayID must range between 0-5.");
         return;
     }
 
@@ -53,7 +108,9 @@ const getWeatherInfo = async (city, day) => {
     return weatherInfo;
 }
 
-
+//city - city name as full or partial string
+//day - array id, days start from current day (0), range 0-5
+//woeID - Where On Earth ID - get via getWoeID
 const getTemp = async (city, day) => {
     let tempInfo = {};
 
@@ -80,34 +137,30 @@ const getTemp = async (city, day) => {
     return tempInfo;
 }
 
-/*
-//woeID - Where On Earth ID - get via getWoeID
-//dayID - array id, dayIDs start from current day (0), range 0-5
-const getWeatherInfo = async (woeID, dayID) => {
-    let weatherInfo = "";
-    if (dayID > 5 || dayID < 0){
-        console.error("DayID must range between 0-5.");
-        return;
-    }
-
-    try {
-        const data = await WeatherRequests.getWeatherRequest(woeID);
-        //console.log(data);
-        weatherInfo = data.consolidated_weather[dayID];
-    } catch (error) {
-        console.error(error);
-    }
-
-    //console.log(weatherInfo);
-    return weatherInfo;
-}
-*/
-
 exports.getWoeID = getWoeID;
-exports.getWeatherInfo = getWeatherInfo;
+exports.getWeatherWithCache = getWeatherWithCache;
+exports.getWeather = getWeather;
 exports.getTemp = getTemp;
+exports.getTempWithCache = getTempWithCache;
 
 
+function getNewCacheTimer(time, timer) {
+    const now = new Date().getTime()
+    if (timer < now + time) {
+        timer = now + time
+    }
+    return timer
+}
+
+async function getCacheTimers(){
+    if (await fs.existsSync(__dirname + '/cache/cacheTimers/weatherInfoCacheTimer.txt')) {
+        weatherInfoCacheTimer = await fs.readFileSync(__dirname + '/cache/cacheTimers/weatherInfoCacheTimer.txt');
+    }
+    if (await fs.existsSync(__dirname + '/cache/cacheTimers/weatherInfoCacheTimer.txt')) {
+        tempCacheTimer = await fs.readFileSync(__dirname + '/cache/cacheTimers/tempCacheTimer.txt');
+    }
+
+} 
 
 //FOR TESTING DIRECTLY
 /*
